a11d948eb684cca78693526425383c22ada1151f
zrk <sdlkzrk1025@163.com>
2020-05-13 17:39:00 +0800

    基础模块微服务

---
+++ sns-base/src/main/java/com/zrkworld/sns/base/BaseApplication.java
@@ -0,0 +1,23 @@
+package com.zrkworld.sns.base;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.tensquare.base.dao*")
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
+    }
+}
---
+++ sns-base/src/main/java/com/zrkworld/sns/base/mapper/LabelMapper.java
@@ -0,0 +1,10 @@
+package com.zrkworld.sns.base.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zrkworld.sns.base.pojo.Label;
+
+/**
+ * label数据接口层
+ */
+public interface LabelMapper extends BaseMapper<Label> {
+}
