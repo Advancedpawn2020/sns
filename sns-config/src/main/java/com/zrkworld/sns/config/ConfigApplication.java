dbb8bd8d3deb632127d3fb892ee903b64d0e627c
zrk <sdlkzrk1025@163.com>
2020-05-28 22:06:51 +0800

    修复pom文件导入异常bug

--- sns-config/src/main/java/com/zrkworld/sns/config/ConfigApplication.java
+++ sns-config/src/main/java/com/zrkworld/sns/config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.tensquare.config;
+package com.zrkworld.sns.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
