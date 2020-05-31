6006097e7f92e6fbbfb5c50d22a72ebcb997a128
zrk <sdlkzrk1025@163.com>
2020-05-31 16:48:25 +0800

    短信发送微服务

---
+++ sns-parent/sns-sms/src/main/java/com/zrkworld/sns/sms/SmsApplication.java
@@ -0,0 +1,12 @@
+package com.zrkworld.sns.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class, args);
+    }
+}
---
+++ sns-parent/sns-sms/src/main/java/com/zrkworld/sns/sms/listener/SmsListener.java
@@ -0,0 +1,32 @@
+package com.zrkworld.sns.sms.listener;
+
+import com.aliyuncs.exceptions.ClientException;
+import com.zrkworld.sns.sms.utils.SmsUtil;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Map;
+import java.util.Random;
+
+@Component
+@RabbitListener(queues = "sms")
+public class SmsListener {
+
+    @Resource
+    private SmsUtil smsUtil;
+
+    @RabbitHandler
+    public void executeSms(Map<String, String> map) {
+        String mobile = map.get("mobile");
+        String checkCode = map.get("check_code");
+        System.out.println("手机号： " + mobile);
+        System.out.println("验证码： " + checkCode);
+        try {
+            smsUtil.sendSms(mobile, "SMS_"+new Random().nextInt(100), "测试签名", "{\"code\": \"" + checkCode + "\"}");
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
