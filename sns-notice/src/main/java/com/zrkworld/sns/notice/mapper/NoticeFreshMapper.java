516122ed342f74fee9407bf382355712e275ea59
zrk <sdlkzrk1025@163.com>
2020-05-31 11:46:25 +0800

    改造工程结构1.0

--- sns-notice/src/main/java/com/zrkworld/sns/notice/mapper/NoticeFreshMapper.java
+++
@@ -1,8 +0,0 @@
-package com.zrkworld.sns.notice.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zrkworld.sns.notice.pojo.NoticeFresh;
-
-public interface NoticeFreshMapper extends BaseMapper<NoticeFresh> {
-}
